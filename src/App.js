4fee479f51f1b321c073b7f946033cd0bb92180b
Diptanu chakraborty <chakrabortyd943@gmail.com>
2021-03-24 12:15:24 +0530

    hjgjg

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Diptanu chakraborty<br></br>
+         Redirect to <a href="https://diptanuchakraborty.in">Homepage</a>
         </p>
         <a
           className="App-link"
